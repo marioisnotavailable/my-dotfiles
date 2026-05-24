22df364a170ff304daac5ee971b5f947a9858a5a
marioisnotavailable <mario.haunold07@gmail.com>
2026-05-24 18:48:48 +0200

    fix: hide battery pill completely on desktops

--- config/ironbar/scripts/battery-check.sh
+++ config/ironbar/scripts/battery-check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if ls /sys/class/power_supply/ | grep -qi '^BAT'; then
-    printf "true"
+if ls /sys/class/power_supply/ 2>/dev/null | grep -qi '^BAT'; then
+    exit 0
 else
-    printf "false"
+    exit 1
 fi
